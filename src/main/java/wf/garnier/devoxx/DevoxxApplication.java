8172d46927a707519a07df03bd6f51af1fa73067
Daniel Garnier-Moiroux <git@garnier.wf>
2022-03-30 21:56:59 +0200

    feature: initial commit, add empty project

---
+++ src/main/java/wf/garnier/devoxx/DevoxxApplication.java
@@ -0,0 +1,13 @@
+package wf.garnier.devoxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoxxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wf/garnier/devoxx/DevoxxApplicationTests.java
@@ -0,0 +1,13 @@
+package wf.garnier.devoxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevoxxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
