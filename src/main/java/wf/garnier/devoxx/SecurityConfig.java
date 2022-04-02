a7a39a8d0079a10aaec1680d66336fd00730c8aa
Daniel Garnier-Moiroux <git@garnier.wf>
2022-04-02 14:35:48 +0200

    can log in with "user"/"password"

--- src/main/java/wf/garnier/devoxx/SecurityConfig.java
+++ src/main/java/wf/garnier/devoxx/SecurityConfig.java
@@ -1,9 +1,14 @@
 package wf.garnier.devoxx;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,7 +22,14 @@ public class SecurityConfig {
 					.antMatchers("/error").permitAll()
 					.antMatchers("/favicon.ico").permitAll()
 					.anyRequest().authenticated()
-				.and()
-				.build();
+				.and().formLogin()
+				.and().build();
+	}
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		return new InMemoryUserDetailsManager(
+				new User("user", "{noop}password", Collections.emptyList())
+		);
 	}
 }
