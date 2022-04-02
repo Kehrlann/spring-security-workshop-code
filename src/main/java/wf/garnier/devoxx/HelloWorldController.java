e728cd3086777cd9dbff53fe524a9ebf7608a341
Daniel Garnier-Moiroux <git@garnier.wf>
2022-04-02 21:10:41 +0200

    oauth2 google username is now fullname in /private

--- src/main/java/wf/garnier/devoxx/HelloWorldController.java
+++ src/main/java/wf/garnier/devoxx/HelloWorldController.java
@@ -1,5 +1,10 @@
 package wf.garnier.devoxx;
 
+import java.util.Optional;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +17,17 @@ public class HelloWorldController {
 	}
 
 	@GetMapping("private")
-	public String privatePage() {
-		return "Mmmmh, this should be private 🤔";
+	public String privatePage(Authentication authentication) {
+		return "Welcome to this very private page, ~[" + getName(authentication) + "]~! 🥳🎉🍾";
 	}
 
+	private String getName(Authentication authentication) {
+		return Optional.of(authentication)
+				.filter(OAuth2AuthenticationToken.class::isInstance)
+				.map(OAuth2AuthenticationToken.class::cast)
+				.map(OAuth2AuthenticationToken::getPrincipal)
+				.map(OidcUser.class::cast)
+				.map(OidcUser::getFullName)
+				.orElseGet(authentication::getName);
+	}
 }
